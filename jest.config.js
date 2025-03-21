24c697b692577bb5cc938e5877ee0e2e80c5fea4
Nimish Chaudhari <37145066+nimishchaudhari@users.noreply.github.com>
2025-03-21 14:33:52 +0100

    Add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      useESM: true,
+    }],
+  },
+  setupFiles: ['dotenv/config']
+};
